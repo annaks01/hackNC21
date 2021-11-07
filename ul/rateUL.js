c3563ebfbaf8465cb7014d9eaf60b5d6790dc01b
annaks01 <annasnyder350@gmail.com>
2021-11-06 22:04:47 -0400

    data stuff

--- ul/rateUL.js
+++ ul/rateUL.js
@@ -1,70 +1,95 @@
-function init() {
-    // gender neutral
-    localStorage.setItem("genderNeutralYes", 0);
-    localStorage.setItem("genderNeutralNo", 0);
-    localStorage.setItem("genderNeutralTotal", 0);
-    localStorage.setItem("genderNeutralPercent", 0);
+localStorage.setItem("initBeenCalled", false);
 
-    // vending machines
-    localStorage.setItem("vendingMachinesYes", 0);
-    localStorage.setItem("vendingMachineNo", 0);
-    localStorage.setItem("vendingMachineTotal", 0);
-    localStorage.setItem("vendingMachinesPercent", 0);
+function initRateUL() {
+    if(!localStorage.getItem("initBeenCalled")) {
+        // gender neutral
+        localStorage.setItem("genderNeutralYes", 0);
+        localStorage.setItem("genderNeutralNo", 0);
+        localStorage.setItem("genderNeutralTotal", 0);
+        localStorage.setItem("genderNeutralPercent", 0);
 
-    // food and drinks
-    localStorage.setItem("foodYes", 0);
-    localStorage.setItem("foodNo", 0);
-    localStorage.setItem("foodTotal", 0);
-    localStorage.setItem("foodPercent", 0);
+        // vending machines
+        localStorage.setItem("vendingMachinesYes", 0);
+        localStorage.setItem("vendingMachineNo", 0);
+        localStorage.setItem("vendingMachineTotal", 0);
+        localStorage.setItem("vendingMachinesPercent", 0);
 
-    // standing desks
-    localStorage.setItem("standingDesksYes", 0);
-    localStorage.setItem("standingDeskslNo", 0);
-    localStorage.setItem("standingDesksTotal", 0);
-    localStorage.setItem("standingDesksPercent", 0);
+        // food and drinks
+        localStorage.setItem("foodYes", 0);
+        localStorage.setItem("foodNo", 0);
+        localStorage.setItem("foodTotal", 0);
+        localStorage.setItem("foodPercent", 0);
 
-    // printers
-    localStorage.setItem("printersYes", 0);
-    localStorage.setItem("printersNo", 0);
-    localStorage.setItem("printersTotal", 0);
-    localStorage.setItem("printersPercent", 0);
+        // standing desks
+        localStorage.setItem("standingDesksYes", 0);
+        localStorage.setItem("standingDeskslNo", 0);
+        localStorage.setItem("standingDesksTotal", 0);
+        localStorage.setItem("standingDesksPercent", 0);
 
-    // copiers
-    localStorage.setItem("copiersYes", 0);
-    localStorage.setItem("copiersNo", 0);
-    localStorage.setItem("copiersTotal", 0);
-    localStorage.setItem("copiersPercent", 0);
+        // printers
+        localStorage.setItem("printersYes", 0);
+        localStorage.setItem("printersNo", 0);
+        localStorage.setItem("printersTotal", 0);
+        localStorage.setItem("printersPercent", 0);
 
-    // phone chargers
-    localStorage.setItem("chargersYes", 0);
-    localStorage.setItem("chargersNo", 0);
-    localStorage.setItem("chargersTotal", 0);
-    localStorage.setItem("chargersPercent", 0);
+        // copiers
+        localStorage.setItem("copiersYes", 0);
+        localStorage.setItem("copiersNo", 0);
+        localStorage.setItem("copiersTotal", 0);
+        localStorage.setItem("copiersPercent", 0);
 
-    // One Card
-    localStorage.setItem("oneCardYes", 0);
-    localStorage.setItem("oneCardNo", 0);
-    localStorage.setItem("oneCardTotal", 0);
-    localStorage.setItem("oneCardPercent", 0);
+        // phone chargers
+        localStorage.setItem("chargersYes", 0);
+        localStorage.setItem("chargersNo", 0);
+        localStorage.setItem("chargersTotal", 0);
+        localStorage.setItem("chargersPercent", 0);
+
+        // One Card
+        localStorage.setItem("oneCardYes", 0);
+        localStorage.setItem("oneCardNo", 0);
+        localStorage.setItem("oneCardTotal", 0);
+        localStorage.setItem("oneCardPercent", 0);
+
+        // changing init var
+        localStorage.setItem("initBeenCalled", true);
+    }  
+    
+    console.log("on load of page 3: " + localStorage.getItem("genderNeutralPercent"));
+}
+
+function initPage2() {
+    console.log("init page 2: " + localStorage.getItem("genderNeutralPercent"));
+    var link = window.location.href;
+    var params = link.split("?");
+    var ratings = new Array(8);
+    for(let i = 1; i <= 8; i++) {
+        console.log(params);
+        var gn = params[i];
+        console.log(gn);
+        var gn2 = gn.split("=")[1];
+        console.log(gn2);
+        var croppedGn = Math.round(Number(gn2) * 100);
+        console.log(croppedGn);
+        ratings[i-1] = croppedGn;
+    }
+    console.log(ratings);
+    document.getElementById("genNeu").innerHTML = ratings[0];
+    document.getElementById("venMac").innerHTML = ratings[1];
+    document.getElementById("foo").innerHTML = ratings[2];
+    document.getElementById("staDes").innerHTML = ratings[3];
+    document.getElementById("pri").innerHTML = ratings[4];
+    document.getElementById("cop").innerHTML = ratings[5];
+    document.getElementById("cha").innerHTML = ratings[6];
+    document.getElementById("oneCar").innerHTML = ratings[7];
+
 }
 
 function submit() {
-    // const yesNoAnswers = [document.getElementById("genderNeutralYes").checked, document.getElementById("vendingMachinesYes").checked, document.getElementById("foodYes").checked, document.getElementById("standingDesksYes").checked, document.getElementById("printersYes").checked, document.getElementById("copiersYes").checked, document.getElementById("chargersYes").checked, document.getElementById("oneCardYes").checked];
-    // console.log(yesNoAnswers);
-
     // gender neutral
     if(document.getElementById("genderNeutralYes").checked) {
-        // console.log(localStorage.genderNeutralYes);
-        // console.log(localStorage.genderNeutralTotal);
-        // console.log(localStorage.genderNeutralPercent);
-
         localStorage.setItem("genderNeutralYes", Number(localStorage.getItem("genderNeutralYes"))+1);
         localStorage.setItem("genderNeutralTotal", Number(localStorage.getItem("genderNeutralTotal"))+1);
         localStorage.setItem("genderNeutralPercent", Number(localStorage.getItem("genderNeutralYes")) / Number(localStorage.getItem("genderNeutralTotal")));
-
-        // console.log(localStorage.genderNeutralYes);
-        // console.log(localStorage.genderNeutralTotal);
-        // console.log(localStorage.genderNeutralPercent);
     }
 
     else if(document.getElementById("genderNeutralNo").checked) {
@@ -129,7 +154,7 @@ function submit() {
     if(document.getElementById("copiersYes").checked) {
         localStorage.setItem("copiersYes", Number(localStorage.getItem("copiersYes"))+1);
         localStorage.setItem("copiersTotal", Number(localStorage.getItem("copiersTotal"))+1);
-        localStorage.setItem("copiers", Number(localStorage.getItem("copiersYes")) / Number(localStorage.getItem("copiersTotal")));
+        localStorage.setItem("copiersPercent", Number(localStorage.getItem("copiersYes")) / Number(localStorage.getItem("copiersTotal")));
     }
 
     else if(document.getElementById("copiersNo").checked) {
@@ -142,7 +167,7 @@ function submit() {
     if(document.getElementById("chargersYes").checked) {
         localStorage.setItem("chargersYes", Number(localStorage.getItem("chargersYes"))+1);
         localStorage.setItem("chargersTotal", Number(localStorage.getItem("chargersTotal"))+1);
-        localStorage.setItem("chargers", Number(localStorage.getItem("chargersYes")) / Number(localStorage.getItem("chargersTotal")));
+        localStorage.setItem("chargersPercent", Number(localStorage.getItem("chargersYes")) / Number(localStorage.getItem("chargersTotal")));
     }
 
     else if(document.getElementById("chargersNo").checked) {
@@ -155,7 +180,7 @@ function submit() {
     if(document.getElementById("oneCardYes").checked) {
         localStorage.setItem("oneCardYes", Number(localStorage.getItem("oneCardYes"))+1);
         localStorage.setItem("oneCardTotal", Number(localStorage.getItem("oneCardTotal"))+1);
-        localStorage.setItem("oneCard", Number(localStorage.getItem("oneCardYes")) / Number(localStorage.getItem("oneCardTotal")));
+        localStorage.setItem("oneCardPercent", Number(localStorage.getItem("oneCardYes")) / Number(localStorage.getItem("oneCardTotal")));
     }
 
     else if(document.getElementById("oneCardNo").checked) {
@@ -164,12 +189,14 @@ function submit() {
         localStorage.setItem("oneCardPercent", Number(localStorage.getItem("oneCardYes")) / Number(localStorage.getItem("oneCardTotal")));
     }
 
-    console.log(localStorage.genderNeutralPercent);
-    console.log(localStorage.vendingMachinesPercent);
-    console.log(localStorage.foodPercent);
-    console.log(localStorage.standingDesksPercent);
-    console.log(localStorage.printersPercent);
-    console.log(localStorage.copiersPercent);
-    console.log(localStorage.chargersPercent);
-    console.log(localStorage.oneCardPercent);
+    // returning to page 2
+    console.log(localStorage.getItem("copiersPercent"));
+    window.location.href="pageUL.html?genderNeutral=" + localStorage.getItem("genderNeutralPercent") + 
+    "?vendingMachines=" + localStorage.getItem("vendingMachinesPercent") + 
+    "?food=" + localStorage.getItem("foodPercent") + 
+    "?standingDesks=" + localStorage.getItem("standingDesksPercent") + 
+    "?printers=" + localStorage.getItem("printersPercent") + 
+    "?copiers=" + localStorage.getItem("copiersPercent") + 
+    "?chargers=" + localStorage.getItem("chargersPercent") + 
+    "?oneCard=" + localStorage.getItem("oneCardPercent");
 }
