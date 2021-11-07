bc304c183f051d79baef0b31af788971fdf41724
annaks01 <annasnyder350@gmail.com>
2021-11-07 01:05:45 -0500

    completely working ul

--- ul/rateUL.js
+++ ul/rateUL.js
@@ -50,6 +50,26 @@ function initRateUL() {
         localStorage.setItem("oneCardTotal", 0);
         localStorage.setItem("oneCardPercent", 0);
 
+        // physical accessibility
+        localStorage.setItem("totalHAPoints",0);
+        localStorage.setItem("totalHARatings", 0);
+        localStorage.setItem("haAverage", 0);
+
+         // atmosphere
+         localStorage.setItem("totalAtmospherePoints",0);
+         localStorage.setItem("totalAtmosphereRatings", 0);
+         localStorage.setItem("atmosphereAverage", 0);
+
+        // physical accessibility
+        localStorage.setItem("totalSupportPoints",0);
+        localStorage.setItem("totalSupportRatings", 0);
+        localStorage.setItem("supportAverage", 0);
+
+         // physical accessibility
+         localStorage.setItem("totalCovidPoints",0);
+         localStorage.setItem("totalCovidRatings", 0);
+         localStorage.setItem("covidAverage", 0);
+
         // changing init var
         localStorage.setItem("initBeenCalled", true);
     }  
@@ -58,7 +78,6 @@ function initRateUL() {
 }
 
 function initPage2() {
-    console.log("init page 2: " + localStorage.getItem("genderNeutralPercent"));
     var link = window.location.href;
 
     if(link.includes("?")) {
@@ -74,6 +93,7 @@ function initPage2() {
             console.log(croppedGn);
             ratings[i-1] = croppedGn;
         }
+
         console.log(ratings);
         document.getElementById("genNeu").innerHTML = ratings[0];
         document.getElementById("venMac").innerHTML = ratings[1];
@@ -92,6 +112,28 @@ function initPage2() {
         localStorage.setItem("copiers", ratings[5]);
         localStorage.setItem("chargers", ratings[6]);
         localStorage.setItem("oneCard", ratings[7]);
+
+        var amenities = new Array(4);
+        for(let i = 9; i <= 12; i++) {
+            console.log(params[i]);
+            var item = params[i];
+            console.log(item);
+            var item2 = item.split("=")[1];
+            console.log(item2);
+            amenities[i-9] = Number(item2);
+        }
+
+        document.getElementById("ha").innerHTML = amenities[0];
+        document.getElementById("a").innerHTML = amenities[1];
+        document.getElementById("s").innerHTML = amenities[2];
+        document.getElementById("c").innerHTML = amenities[3];
+
+        localStorage.setItem("haAverage", amenities[0]);
+        localStorage.setItem("atmosphereAverage", amenities[1]);
+        localStorage.setItem("supportAverage", amenities[2]);
+        localStorage.setItem("covidAverage", amenities[3]);
+
+
     }
 
     else {
@@ -103,11 +145,15 @@ function initPage2() {
         document.getElementById("cop").innerHTML = localStorage.getItem("copiers");
         document.getElementById("cha").innerHTML = localStorage.getItem("chargers");
         document.getElementById("oneCar").innerHTML = localStorage.getItem("oneCard");
+
+        document.getElementById("ha").innerHTML = localStorage.getItem("haAverage");
+        document.getElementById("a").innerHTML = localStorage.getItem("atmosphereAverage");
+        document.getElementById("s").innerHTML = localStorage.getItem("supportAverage");
+        document.getElementById("c").innerHTML = localStorage.getItem("covidAverage");
     }
 }
 
 function submit() {
-
     // gender neutral
     if(document.getElementById("genderNeutralYes").checked) {
         localStorage.setItem("genderNeutralYes", Number(localStorage.getItem("genderNeutralYes"))+1);
@@ -212,8 +258,118 @@ function submit() {
         localStorage.setItem("oneCardPercent", Number(localStorage.getItem("oneCardYes")) / Number(localStorage.getItem("oneCardTotal")));
     }
 
+    // physical accessibility
+    if(document.getElementById("accessibilityStar5").checked) {
+        console.log("checkeeeeeeeddddddddddddd!");
+        localStorage.setItem("totalHAPoints", Number(localStorage.getItem("totalHAPoints")) + 5);
+        localStorage.setItem("totalHARatings", Number(localStorage.getItem("totalHARatings")) + 1);
+    }
+
+    else if(document.getElementById("accessibilityStar4").checked) {
+        localStorage.setItem("totalHAPoints", Number(localStorage.getItem("totalHAPoints")) + 4);
+        localStorage.setItem("totalHARatings", Number(localStorage.getItem("totalHARatings")) + 1);
+    }
+
+    else if(document.getElementById("accessibilityStar3").checked) {
+        localStorage.setItem("totalHAPoints", Number(localStorage.getItem("totalHAPoints")) + 3);
+        localStorage.setItem("totalHARatings", Number(localStorage.getItem("totalHARatings")) + 1);
+    }
+
+    else if(document.getElementById("accessibilityStar2").checked) {
+        localStorage.setItem("totalHAPoints", Number(localStorage.getItem("totalHAPoints")) + 2);
+        localStorage.setItem("totalHARatings", Number(localStorage.getItem("totalHARatings")) + 1);
+    }
+
+    else if(document.getElementById("accessibilityStar1").checked) {
+        localStorage.setItem("totalHAPoints", Number(localStorage.getItem("totalHAPoints")) + 1);
+        localStorage.setItem("totalHARatings", Number(localStorage.getItem("totalHARatings")) + 1);
+    }
+
+    // atmosphere
+    if(document.getElementById("atmosphereStar5").checked) {
+        localStorage.setItem("totalAtmospherePoints", Number(localStorage.getItem("totalAtmospherePoints")) + 5);
+        localStorage.setItem("totalAtmosphereRatings", Number(localStorage.getItem("totalAtmosphereRatings")) + 1);
+    }
+
+    else if(document.getElementById("atmosphereStar4").checked) {
+        localStorage.setItem("totalAtmospherePoints", Number(localStorage.getItem("totalAtmospherePoints")) + 4);
+        localStorage.setItem("totalAtmosphereRatings", Number(localStorage.getItem("totalAtmosphereRatings")) + 1);
+    }
+
+    else if(document.getElementById("atmosphereStar3").checked) {
+        localStorage.setItem("totalAtmospherePoints", Number(localStorage.getItem("totalAtmospherePoints")) + 3);
+        localStorage.setItem("totalAtmosphereRatings", Number(localStorage.getItem("totalAtmosphereRatings")) + 1);
+    }
+
+    else if(document.getElementById("atmosphereStar2").checked) {
+        localStorage.setItem("totalAtmospherePoints", Number(localStorage.getItem("totalAtmospherePoints")) + 2);
+        localStorage.setItem("totalAtmosphereRatings", Number(localStorage.getItem("totalAtmosphereRatings")) + 1);
+    }
+
+    else if(document.getElementById("atmosphereStar1").checked) {
+        localStorage.setItem("totalAtmospherePoints", Number(localStorage.getItem("totalAtmospherePoints")) + 1);
+        localStorage.setItem("totalAtmosphereRatings", Number(localStorage.getItem("totalAtmosphereRatings")) + 1);
+    }
+
+    // support
+    if(document.getElementById("supportStar5").checked) {
+        localStorage.setItem("totalSupportPoints", Number(localStorage.getItem("totalSupportPoints")) + 5);
+        localStorage.setItem("totalSupportRatings", Number(localStorage.getItem("totalSupportRatings")) + 1);
+    }
+
+    else if(document.getElementById("supportStar4").checked) {
+        localStorage.setItem("totalSupportPoints", Number(localStorage.getItem("totalSupportPoints")) + 4);
+        localStorage.setItem("totalSupportRatings", Number(localStorage.getItem("totalSupportRatings")) + 1);
+    }
+
+    else if(document.getElementById("supportStar3").checked) {
+        localStorage.setItem("totalSupportPoints", Number(localStorage.getItem("totalSupportPoints")) + 3);
+        localStorage.setItem("totalSupportRatings", Number(localStorage.getItem("totalSupportRatings")) + 1);
+    }
+
+    else if(document.getElementById("supportStar2").checked) {
+        localStorage.setItem("totalSupportPoints", Number(localStorage.getItem("totalSupportPoints")) + 2);
+        localStorage.setItem("totalSupportRatings", Number(localStorage.getItem("totalSupportRatings")) + 1);
+    }
+
+    else if(document.getElementById("supportStar1").checked) {
+        localStorage.setItem("totalSupportPoints", Number(localStorage.getItem("totalSupportPoints")) + 1);
+        localStorage.setItem("totalSupportRatings", Number(localStorage.getItem("totalSupportRatings")) + 1);
+    }
+
+    // covid
+    if(document.getElementById("covidStar5").checked) {
+        localStorage.setItem("totalCovidPoints", Number(localStorage.getItem("totalCovidPoints")) + 5);
+        localStorage.setItem("totalCovidRatings", Number(localStorage.getItem("totalCovidRatings")) + 1);
+    }
+
+    else if(document.getElementById("covidStar4").checked) {
+        localStorage.setItem("totalCovidPoints", Number(localStorage.getItem("totalCovidPoints")) + 4);
+        localStorage.setItem("totalCovidRatings", Number(localStorage.getItem("totalCovidRatings")) + 1);
+    }
+
+    else if(document.getElementById("covidStar3").checked) {
+        localStorage.setItem("totalCovidPoints", Number(localStorage.getItem("totalCovidPoints")) + 3);
+        localStorage.setItem("totalCovidRatings", Number(localStorage.getItem("totalCovidRatings")) + 1);
+    }
+
+    else if(document.getElementById("covidStar2").checked) {
+        localStorage.setItem("totalCovidPoints", Number(localStorage.getItem("totalCovidPoints")) + 2);
+        localStorage.setItem("totalCovidRatings", Number(localStorage.getItem("totalCovidRatings")) + 1);
+    }
+
+    else if(document.getElementById("covidStar1").checked) {
+        localStorage.setItem("totalCovidPoints", Number(localStorage.getItem("totalCovidPoints")) + 1);
+        localStorage.setItem("totalCovidRatings", Number(localStorage.getItem("totalCovidRatings")) + 1);
+    }
+
+    // resetting all averages
+    localStorage.setItem("haAverage", Number(localStorage.getItem("totalHAPoints"))/Number(localStorage.getItem("totalHARatings")));
+    localStorage.setItem("atmosphereAverage", Number(localStorage.getItem("totalAtmospherePoints")/localStorage.getItem("totalAtmosphereRatings")));
+    localStorage.setItem("supportAverage", Number(localStorage.getItem("totalSupportPoints")/localStorage.getItem("totalSupportRatings")));
+    localStorage.setItem("covidAverage", Number(localStorage.getItem("totalCovidPoints")/localStorage.getItem("totalCovidRatings")));
+
     // returning to page 2
-    console.log(localStorage.getItem("copiersPercent"));
     window.location.href="pageUL.html?genderNeutral=" + localStorage.getItem("genderNeutralPercent") + 
     "?vendingMachines=" + localStorage.getItem("vendingMachinesPercent") + 
     "?food=" + localStorage.getItem("foodPercent") + 
@@ -221,5 +377,9 @@ function submit() {
     "?printers=" + localStorage.getItem("printersPercent") + 
     "?copiers=" + localStorage.getItem("copiersPercent") + 
     "?chargers=" + localStorage.getItem("chargersPercent") + 
-    "?oneCard=" + localStorage.getItem("oneCardPercent");
+    "?oneCard=" + localStorage.getItem("oneCardPercent") +
+    "?accessibility=" + localStorage.getItem("haAverage") +
+    "?atmosphere=" + localStorage.getItem("atmosphereAverage") + 
+    "?support=" + localStorage.getItem("supportAverage") +
+    "?covid=" + localStorage.getItem("covidAverage");
 }
